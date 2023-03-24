019c679cb7d6eee1b1c0d886d292de03c84a1b2a
silentFUSU <97568721+silentFUSU@users.noreply.github.com>
2023-03-24 10:47:21 +0800

    Update postprocess.py

--- postprocess.py
+++ postprocess.py
@@ -1,15 +1,27 @@
 import pysam
 
 bamfile = pysam.AlignmentFile('bam.bam', 'rb')
-cell_counts = {}
-for read in bamfile.fetch('chr21', 8214763, 8214764):
-    if read.query_sequence[read.query_alignment_start] == 'G':
-        if read.has_tag('CB'):
-            cell_id = read.get_tag('CB')
-            cell_counts[cell_id] = cell_counts.get(cell_id, 0) + 1
 
+with open("snp_position.txt", "r") as f:
+    for line in f:
+        fields = line.strip().split()
+        mutation_name = "-".join(fields[:4])
+        cell_counts = {}
+        for read in bamfile.fetch(fields[0],int(fields[1])-1,int(fields[1])):
+            if read.query_sequence[read.query_alignment_start] == fields[2]:
+                if read.has_tag('CB'):
+                    cell_id = read.get_tag('CB')
+                    cell_counts[cell_id] = cell_counts.get(cell_id, 0) + 1
+        with open(f'{mutation_name}-pri1.txt', 'w') as f:
+            for cell_id, count in cell_counts.items():
+                f.write(cell_id + '\n') 
+        cell_counts = {}
+        for read in bamfile.fetch(fields[0],int(fields[1])-1,int(fields[1])):
+            if read.query_sequence[read.query_alignment_start] == fields[3]:
+                if read.has_tag('CB'):
+                    cell_id = read.get_tag('CB')
+                    cell_counts[cell_id] = cell_counts.get(cell_id, 0) + 1
+        with open(f'{mutation_name}-pri2.txt', 'w') as f:
+            for cell_id, count in cell_counts.items():
+                f.write(cell_id + '\n') 
 bamfile.close()
-
-with open('cell_ids_rec.txt', 'w') as f:
-    for cell_id, count in cell_counts.items():
-        f.write(cell_id + '\n')
