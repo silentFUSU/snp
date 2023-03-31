d92f55c2b5b2e4613d66eb9b56d9ea2ff6ba2810
silentFUSU <97568721+silentFUSU@users.noreply.github.com>
2023-03-31 15:19:12 +0800

    Update postprocess.py

--- postprocess.py
+++ postprocess.py
@@ -1,8 +1,21 @@
 import pysam
+import argparse
+import os
+parser = argparse.ArgumentParser()
+parser.add_argument("-bf","--bamfile" , help="the path to a bam file")
+parser.add_argument("-tf","--txtfile" , help="the path to the snp txt file")
+parser.add_argument("-sample", help="sample name")
+parser.add_argument("-condition", help="sample condition")
 
-bamfile = pysam.AlignmentFile('bam.bam', 'rb')
+args = parser.parse_args()
+bamfile = pysam.AlignmentFile('{}'.format(args.bamfile), 'rb')
+result_path='/storage/zhangyanxiaoLab/suzhuojie/projects/medulloblastoma/result/merge_data/before_doubletfinder/snp'
+outputdir='{}/snp_{}_{}'.format(result_path,args.sample,args.condition)
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
 
-with open("snp_position.txt", "r") as f:
+#1 ref 2 alt
+with open('{}'.format(args.txtfile), "r") as f:
     for line in f:
         fields = line.strip().split()
         mutation_name = "-".join(fields[:4])
@@ -12,7 +25,7 @@ with open("snp_position.txt", "r") as f:
                 if read.has_tag('CB'):
                     cell_id = read.get_tag('CB')
                     cell_counts[cell_id] = cell_counts.get(cell_id, 0) + 1
-        with open(f'{mutation_name}-pri1.txt', 'w') as f:
+        with open('{}/snp_{}_{}/{}-{}1.txt'.format(result_path,args.sample,args.condition,mutation_name,args.condition), 'w') as f:
             for cell_id, count in cell_counts.items():
                 f.write(cell_id + '\n') 
         cell_counts = {}
@@ -21,7 +34,8 @@ with open("snp_position.txt", "r") as f:
                 if read.has_tag('CB'):
                     cell_id = read.get_tag('CB')
                     cell_counts[cell_id] = cell_counts.get(cell_id, 0) + 1
-        with open(f'{mutation_name}-pri2.txt', 'w') as f:
+        with open('{}/snp_{}_{}/{}-{}2.txt'.format(result_path,args.sample,args.condition,mutation_name,args.condition), 'w') as f:
             for cell_id, count in cell_counts.items():
                 f.write(cell_id + '\n') 
+
 bamfile.close()
